62c8060c37c42eeb5f5fcca8bb28bea67763f3e2
Aravind Prakash Venkatakrishna <araprak007@gmail.com>
2023-08-22 19:58:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// Function to validate input and allow only numeric values
+function validateInput(inputElement) {
+    inputElement.value = inputElement.value.replace(/[^0-9.-]/g, "");
+}
+
+// calculatePower function that calculates and displays the result
+var calculatePower = () => {
+    var base = parseFloat(document.getElementById('base').value);
+    var exponent = parseFloat(document.getElementById('exponent').value);
+
+    if (isNaN(base) || isNaN(exponent)) {
+        var resultDiv = document.getElementById('result');
+        resultDiv.innerHTML = "Please enter valid numeric values for base and exponent.";
+        return;
+    }
+
+    var result = Math.pow(base, exponent);
+
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `Math Result: ${base} ^ ${exponent} = ${result}`;
+
+    // Call the API function
+    callAPI();
+};
+
 // callAPI function that takes the base and exponent numbers as parameters
 var callAPI = () => {
     var base = parseFloat(document.getElementById('base').value);
@@ -15,25 +40,11 @@ var callAPI = () => {
         redirect: 'follow'
     };
 
-    fetch("https://a4tb3iadnh.execute-api.ap-south-1.amazonaws.com/dev/", requestOptions)
+    fetch("Has to be updated", requestOptions)
         .then(response => response.text())
         .then(result => {
             var resultDiv = document.getElementById('result');
             resultDiv.innerHTML = `API Result: ${base} ^ ${exponent} = ${JSON.parse(result).body}`;
         })
         .catch(error => console.log('error', error));
-};
-
-// calculatePower function that calculates and displays the result
-var calculatePower = () => {
-    var base = parseFloat(document.getElementById('base').value);
-    var exponent = parseFloat(document.getElementById('exponent').value);
-
-    var result = Math.pow(base, exponent);
-
-    var resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `Math Result: ${base} ^ ${exponent} = ${result}`;
-
-    // Call the API function
-    callAPI();
 };
