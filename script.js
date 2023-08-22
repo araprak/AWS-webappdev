fe05b3161d43edd6c1a781c845c16a2da535c180
Aravind Prakash Venkatakrishna <araprak007@gmail.com>
2023-08-22 19:51:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 // callAPI function that takes the base and exponent numbers as parameters
 var callAPI = () => {
-    var base = document.getElementById('base').value;
-    var exponent = document.getElementById('exponent').value;
-    
+    var base = parseFloat(document.getElementById('base').value);
+    var exponent = parseFloat(document.getElementById('exponent').value);
+
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    
+
     var raw = JSON.stringify({"base": base, "exponent": exponent});
-    
+
     var requestOptions = {
         method: 'POST',
         headers: myHeaders,
         body: raw,
         redirect: 'follow'
     };
-    
+
     fetch("https://a4tb3iadnh.execute-api.ap-south-1.amazonaws.com/dev/", requestOptions)
         .then(response => response.text())
-        .then(result => alert(JSON.parse(result).body))
+        .then(result => {
+            var resultDiv = document.getElementById('result');
+            resultDiv.innerHTML = `API Result: ${base} ^ ${exponent} = ${JSON.parse(result).body}`;
+        })
         .catch(error => console.log('error', error));
+};
+
+// calculatePower function that calculates and displays the result
+var calculatePower = () => {
+    var base = parseFloat(document.getElementById('base').value);
+    var exponent = parseFloat(document.getElementById('exponent').value);
+
+    var result = Math.pow(base, exponent);
+
+    var resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = `Math Result: ${base} ^ ${exponent} = ${result}`;
+
+    // Call the API function
+    callAPI();
 };
