96c7da47ad9a6660c42d033c27b084f6da591a24
Aravind Prakash Venkatakrishna <araprak007@gmail.com>
2023-08-22 18:00:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-// callAPI function that takes the base and exponent numbers as parameters
+// callAPI function that handles input validation and makes the API call
 var callAPI = () => {
-    var base = document.getElementById('base').value;
-    var exponent = document.getElementById('exponent').value;
+    var baseInput = document.getElementById('base').value;
+    var exponentInput = document.getElementById('exponent').value;
+
+    // Validate input to ensure they are numbers
+    if (!isNumber(baseInput) || !isNumber(exponentInput)) {
+        alert("Please enter valid numeric values.");
+        return;
+    }
+
+    var base = parseFloat(baseInput);
+    var exponent = parseFloat(exponentInput);
 
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({"base":base,"exponent":exponent});
+    var raw = JSON.stringify({"base": base, "exponent": exponent});
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
         method: 'POST',
@@ -21,4 +30,9 @@ var callAPI = () => {
     .then(response => response.text())
     .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error));
+}
+
+// Function to check if a value is a number
+function isNumber(value) {
+    return !isNaN(parseFloat(value)) && isFinite(value);
 }
